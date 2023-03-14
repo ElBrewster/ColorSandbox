abe8321e92bccf61d90581ca2c9150332c669755
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-14 11:17:10 -0700

    Add some styling fixes and sass variable fixes

---
+++ colorbox/src/window/Window.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./Window.scss";
+
+export default function Window(){
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const watchHeight = () => setWindowHeight(window.innerHeight);
+
+        window.addEventListener("resize", watchHeight);
+
+        return function() {
+            window.removeEventListener("resize", watchHeight)
+        }
+    }, [windowHeight])
+
+    useEffect(() => {
+        window.addEventListener("resize", () => setWindowWidth(window.innerWidth))
+    }, [windowWidth])
+
+    console.log("window height", windowHeight)
+    console.log("window width", windowWidth)
+    return(
+        <section className="window-size-box">
+            <h4 className="sizer">Window Sizer Checker</h4>
+            <p className="window-height">{windowHeight}<span className="px-span">&nbsp;px</span></p>
+            <p className="window-width">{windowWidth}<span className="px-span">&nbsp;px</span></p>
+        </section>
+    )
+}
+
+//thanks and credit to Bob Ziroll for some of this walkthrough <3
