cf4264271f64ba50df6d211bffbb9f4720b18e24
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-18 09:25:59 -0700

    Add half data set to htmlColorNames140

---
+++ ColorSandbox/src/data/htmlColorNames140.jsx
@@ -0,0 +1,434 @@
+const htmlColorNames140 = [
+    {
+        id: 1000,
+        name: "Crimson",
+        hex: "#DC143C"
+    },
+    {
+        id: 1001,
+        name: "Royal Red",
+        hex: "#D00060"
+    },
+    {
+        id: 1002,
+        name: "Light Coral",
+        hex: "#F08080"
+    },
+    {
+        id: 1003,
+        name: "I***** Red",
+        hex: "#CD5C5C"
+    },
+    {
+        id: 1004,
+        name: "Fire Brick",
+        hex: "#B22222"
+    },
+    {
+        id: 1005,
+        name: "Brown",
+        hex: "#A52A2A"
+    },
+    {
+        id: 1006,
+        name: "Red",
+        hex: "#FF0000"
+    },
+    {
+        id: 1007,
+        name: "Light Red",
+        hex: "#FF3333"
+    },
+    {
+        id: 1008,
+        name: "Medium Red",
+        hex: "#B10304"
+    },
+    {
+        id: 1009,
+        name: "Dark Red",
+        hex: "#8B0000"
+    },
+    {
+        id: 1010,
+        name: "Deep Red",
+        hex: "#850101"
+    },
+    {
+        id: 1011,
+        name: "Maroon",
+        hex: "#800000"
+    },
+    {
+        id: 1012,
+        name: "Salmon",
+        hex: "#FA8072"
+    },
+    {
+        id: 1013,
+        name: "Red Orange",
+        hex: "#FF5349"
+    },
+    {
+        id: 1014,
+        name: "Light Pink",
+        hex: "#FFB6C1"
+    },
+    {
+        id: 1015,
+        name: "Pink",
+        hex: "#FD6C9E"
+    },
+    {
+        id: 1016,
+        name: "Medium Pink",
+        hex: "#F36196"
+    },
+    {
+        id: 1017,
+        name: "Dark Pink",
+        hex: "#E75480"
+    },
+    {
+        id: 1018,
+        name: "Pale Violet Red",
+        hex: "#DB7093"
+    },
+    {
+        id: 1019,
+        name: "Hot Pink",
+        hex: "#FF69B4"
+    },
+    {
+        id: 1020,
+        name: "Deep Pink",
+        hex: "#FF1493"
+    },
+    {
+        id: 1021,
+        name: "Peach Puff",
+        hex: "#FFDAB9"
+    },
+    {
+        id: 1022,
+        name: "Dark Salmon",
+        hex: "#E9967A"
+    },
+    {
+        id: 1023,
+        name: "Light Salmon",
+        hex: "#FFA07A"
+    },
+    {
+        id: 1024,
+        name: "Coral",
+        hex: "#FF7F50"
+    },
+    {
+        id: 1025,
+        name: "Tomato",
+        hex: "#FF6347"
+    },
+    {
+        id: 1026,
+        name: "OrangeRed",
+        hex: "#FF4500"
+    },
+    {
+        id: 1027,
+        name: "Papaya Whip",
+        hex: "#FFEFD5"
+    },
+    {
+        id: 1028,
+        name: "Moccasin",
+        hex: "#FFE4B5"
+    },
+    {
+        id: 1029,
+        name: "Light Orange",
+        hex: "#D9A465"
+    },
+    {
+        id: 1030,
+        name: "Medium Orange",
+        hex: "#FF7802"
+    },
+    {
+        id: 1031,
+        name: "Dark Orange",
+        hex: "#FF8C00"
+    },
+    {
+        id: 1032,
+        name: "Orange",
+        hex: "#FFA500"
+    },
+    {
+        id: 1033,
+        name: "Yellow Orange",
+        hex: "#FFAE42"
+    },
+    {
+        id: 1034,
+        name: "Light Goldenrod Yellow",
+        hex: "#FAFAD2"
+    },
+    {
+        id: 1035,
+        name: "Pale Goldenrod",
+        hex: "#EEE8AA"
+    },
+    {
+        id: 1036,
+        name: "Goldenrod",
+        hex: "#DAA520"
+    },
+    {
+        id: 1037,
+        name: "Dark Goldenrod",
+        hex: "#B8860B"
+    },
+    {
+        id: 1038,
+        name: "Light Gold",
+        hex: "#F1E5AC"
+    },
+    {
+        id: 1039,
+        name: "Gold",
+        hex: "#FFD700"
+    },
+    {
+        id: 1040,
+        name: "Dark Gold",
+        hex: "#EEBC1D"
+    },
+    {
+        id: 1041,
+        name: "Lemon Chiffon",
+        hex: "#FFFACD"
+    },
+    {
+        id: 1042,
+        name: "Light Yellow",
+        hex: "#FFFFE0"
+    },
+    {
+        id: 1043,
+        name: "Yellow",
+        hex: "#FFFF00"
+    },
+    {
+        id: 1044,
+        name: "Medium Yellow",
+        hex: "#FFE302"
+    },
+    {
+        id: 1045,
+        name: "Dark Yellow",
+        hex: "#FFCC00"
+    },
+    {
+        id: 1046,
+        name: "Olive",
+        hex: "#808000"
+    },
+    {
+        id: 1047,
+        name: "Khaki",
+        hex: "#F0E68C"
+    },
+    {
+        id: 1048,
+        name: "Dark Khaki",
+        hex: "#BDB76B"
+    },
+    {
+        id: 1049,
+        name: "Lavender",
+        hex: "#E6E6FA"
+    },
+    {
+        id: 1050,
+        name: "Medium Purple",
+        hex: "#9370DB"
+    },
+    {
+        id: 1051,
+        name: "Blue Violet",
+        hex: "#8A2BE2"
+    },
+    {
+        id: 1052,
+        name: "Light Violet",
+        hex: "#7A5299"
+    },
+    {
+        id: 1053,
+        name: "Medium Violet",
+        hex: "#65315F"
+    },
+    {
+        id: 1054,
+        name: "Dark Violet",
+        hex: "#9400D3"
+    },
+    {
+        id: 1055,
+        name: "Indigo",
+        hex: "#4B0082"
+    },
+    {
+        id: 1056,
+        name: "Medium Orchid",
+        hex: "#BA55D3"
+    },
+    {
+        id: 1057,
+        name: "Dark Orchid",
+        hex: "#9932CC"
+    },
+    {
+        id: 1058,
+        name: "Thistle",
+        hex: "#D8BFD8"
+    },
+    {
+        id: 1059,
+        name: "Plum",
+        hex: "#DDA0DD"
+    },
+    {
+        id: 1060,
+        name: "Violet",
+        hex: "#EE82EE"
+    },
+    {
+        id: 1061,
+        name: "Orchid",
+        hex: "#DA70D6"
+    },
+    {
+        id: 1062,
+        name: "Fuchsia/Magenta",
+        hex: "#FF00FF"
+    },
+    {
+        id: 1063,
+        name: "Light Magenta",
+        hex: "#FF77FF"
+    },
+    {
+        id: 1064,
+        name: "Dark Magenta",
+        hex: "#8B008B"
+    },
+    {
+        id: 1065,
+        name: "Purple",
+        hex: "#800080"
+    },
+    {
+        id: 1066,
+        name: "Medium Violet Red",
+        hex: "#C71585"
+    },
+    {
+        id: 1067,
+        name: "Yellow Green",
+        hex: "#9ACD32"
+    },
+    {
+        id: 1068,
+        name: "Olive Drab",
+        hex: "#6B8E23"
+    },
+    {
+        id: 1069,
+        name: "Dark Olive Green",
+        hex: "#556B2F"
+    },
+    {
+        id: 1070,
+        name: "Green Yellow",
+        hex: "#ADFF2F"
+    },
+    {
+        id: 1071,
+        name: "Chartreuse",
+        hex: "#7FFF00"
+    },
+    {
+        id: 1072,
+        name: "Lawn Green",
+        hex: "#7CFC00"
+    },
+    {
+        id: 1073,
+        name: "Lime",
+        hex: "#00FF00"
+    },
+    {
+        id: 1074,
+        name: "Green",
+        hex: "#008000"
+    },
+    {
+        id: 1075,
+        name: "Dark Green",
+        hex: "#006400"
+    },
+    {
+        id: 1076,
+        name: "Medium Green",
+        hex: "#037949"
+    },
+    {
+        id: 1077,
+        name: "Pale Green",
+        hex: "#98FB98"
+    },
+    {
+        id: 1078,
+        name: "Light Green",
+        hex: "#90EE90"
+    },
+    {
+        id: 1079,
+        name: "Lime Green",
+        hex: "#32CD32"
+    },
+    {
+        id: 1080,
+        name: "Forest Green",
+        hex: "#228B22"
+    },
+    {
+        id: 1081,
+        name: "Medium Spring Green",
+        hex: "#00FA9A"
+    },
+    {
+        id: 1082,
+        name: "Spring Green",
+        hex: "#00FF7F"
+    },
+    {
+        id: 1083,
+        name: "Medium Sea Green",
+        hex: "#3CB371"
+    },
+    {
+        id: 1084,
+        name: "Sea Green",
+        hex: "#2E8B57"
+    },
+    {
+        id: 1085,
+        name: "Dark Sea Green",
+        hex: "#8FBC8F"
+    }
+];
+
+export default htmlColorNames140;
