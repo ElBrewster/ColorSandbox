be4037ec17a188dd78272e454797e00f4451eef5
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-05-23 14:58:04 -0700

    Add grid display at 433px media query to fix breakpoint

--- ColorSandbox/src/components/window/Window.jsx
+++ ColorSandbox/src/components/window/Window.jsx
@@ -1,31 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./Window.scss";
 
-export default function Window(){
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+export default function Window() {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        const watchHeight = () => setWindowHeight(window.innerHeight);
+  useEffect(() => {
+    const watchHeight = () => setWindowHeight(window.innerHeight);
 
-        window.addEventListener("resize", watchHeight);
+    window.addEventListener("resize", watchHeight);
 
-        return function() {
-            window.removeEventListener("resize", watchHeight)
-        }
-    }, [windowHeight])
+    return function () {
+      window.removeEventListener("resize", watchHeight);
+    };
+  }, [windowHeight]);
 
-    useEffect(() => {
-        window.addEventListener("resize", () => setWindowWidth(window.innerWidth))
-    }, [windowWidth])
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, [windowWidth]);
 
-    return(
-        <section className="window-size-box">
-            <h4 className="sizer">Window Sizer Checker</h4>
-            <p className="window-height">{windowHeight}<span className="px-span">&nbsp;px</span></p>
-            <p className="window-width">{windowWidth}<span className="px-span">&nbsp;px</span></p>
-        </section>
-    )
+  return (
+    <section className="window-size-box">
+      <h4 className="sizer">Window Sizer Checker</h4>
+      <p className="window-height">
+        {windowHeight}
+        <span className="px-span">&nbsp;px</span>
+        &nbsp;height
+      </p>
+      <p className="window-width">
+        {windowWidth}
+        <span className="px-span">&nbsp;px</span>
+        &nbsp;width
+      </p>
+    </section>
+  );
 }
 
 //thanks and credit to Bob Ziroll for some of this walkthrough <3
